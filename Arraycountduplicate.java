ffc9db2928e91e99992e48f75657e009c34f267e
Poovendhan1989 <119952516+Poovendhan1989@users.noreply.github.com>
2022-12-23 12:55:42 +0530

    Add files via upload

---
+++ Arraycountduplicate.java
@@ -0,0 +1,17 @@
+public class Arraycountduplicate
+{
+public static void main(String args[])
+{
+int a[] = {7, 5, 3, 5, 9};
+int search = 5;
+int counter = 0;
+for(int i=0; i<a.length; i++)
+{
+if(a[i] == search)
+{
+counter = counter + 1;
+}
+}
+System.out.println(search + " is present " + counter + " times ");
+}
+}
